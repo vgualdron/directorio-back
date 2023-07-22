1212c083fe4267c53f5e86767f442bac3d5d14c7
victor.gualdron.r@gmail.com <vgualdron@estratec.local>
2023-07-22 00:57:15 -0500

    add change image profile

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -128,4 +128,39 @@ class UserController extends Controller
             'message' => 'Succeed'
         ], JsonResponse::HTTP_OK);
     }
+
+    protected function getB64Image($base64_image)
+    {  
+         // Obtener el String base-64 de los datos         
+         $image_service_str = substr($base64_image, strpos($base64_image, ",")+1);
+         // Decodificar ese string y devolver los datos de la imagen        
+         $image = base64_decode($image_service_str);   
+         // Retornamos el string decodificado
+         return $image; 
+    }
+    
+    protected function getB64Extension($base64_image, $full=null){  
+        // Obtener mediante una expresión regular la extensión imagen y guardarla
+        // en la variable "img_extension"        
+        preg_match("/^data:image\/(.*);base64/i",$base64_image, $img_extension);   
+        // Dependiendo si se pide la extensión completa o no retornar el arreglo con
+        // los datos de la extensión en la posición 0 - 1
+        return ($full) ?  $img_extension[0] : $img_extension[1];  
+    }
+    
+    public function changeImageProfile(Request $request, $id)
+    {
+        // Obtener los datos de la imagen
+        $image_avatar_b64 = $request->image;
+        $img = $this->getB64Image($image_avatar_b64);
+        // Obtener la extensión de la Imagen
+        $img_extension = $this->getB64Extension($image_avatar_b64);
+        // Crear un nombre aleatorio para la imagen
+        $img_name = $id . ''. '.' . $img_extension;
+        // echo $image_name;
+        // Usando el Storage guardar en el disco creado anteriormente y pasandole a 
+        // la función "put" el nombre de la imagen y los datos de la imagen como 
+        // segundo parametro
+        Storage::disk('profile')->put($img_name, $img);
+    }
 }
