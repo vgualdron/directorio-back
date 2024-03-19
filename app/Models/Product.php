323e0bcdbd89b08b6e8dfe0c5afe4e8d032ebc63
victor.gualdron.r@gmail.com <vgualdron@estratec.local>
2024-03-19 01:20:57 -0500

    add product table

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -26,6 +26,13 @@ class Product extends Authenticatable
     ];
 
     /**
+     * The attributes that should be hidden for serialization.
+     *
+     * @var array<int, string>
+     */
+    protected $hidden = [];
+
+    /**
      * The attributes that should be cast.
      *
      * @var array<string, string>
