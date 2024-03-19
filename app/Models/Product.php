eec1bdc2d64af9f4278a0769b6c7e7f6fff263dd
victor.gualdron.r@gmail.com <vgualdron@estratec.local>
2024-03-19 01:18:17 -0500

    add product table

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -25,4 +25,13 @@ class Product extends Authenticatable
         'categorie_id',
     ];
 
+    /**
+     * The attributes that should be cast.
+     *
+     * @var array<string, string>
+     */
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+    ];
+
 }
