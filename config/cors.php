a4c5a941216d56dd7c1f2707cd57d8d983bc3009
victor.gualdron.r@gmail.com <vgualdron@estratec.local>
2024-03-20 03:22:10 -0500

    resolve bug lineClamp ---> 12

--- config/cors.php
+++ config/cors.php
@@ -15,7 +15,7 @@ return [
     |
     */
 
-    'paths' => ['api/*', 'sanctum/csrf-cookie','public/storage/images/*','/public/storage/images/*'],
+    'paths' => ['api/*', 'sanctum/csrf-cookie'],
 
     'allowed_methods' => ['*'],
 
