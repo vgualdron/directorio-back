92724a6a3aa5d51a1703baa961228c2de3fb6768
victor.gualdron.r@gmail.com <vgualdron@estratec.local>
2024-03-20 18:37:17 -0500

    add model imagesproduct

--- routes/api.php
+++ routes/api.php
@@ -54,6 +54,14 @@ Route::group(["prefix" => "/v1", "middleware" => ["auth:sanctum"]], function () 
         Route::delete('/{id}', [CategoryController::class, 'destroy']);
     });
     
+    Route::group(['prefix'=>'/product'], function () {
+        Route::get('/', [ProductController::class, 'index']);
+        Route::get('/{id}', [ProductController::class, 'show']);
+        Route::post('/', [ProductController::class, 'store']);
+        Route::put('/{id}', [ProductController::class, 'update']);
+        Route::delete('/{id}', [ProductController::class, 'destroy']);
+    });
+
     Route::group(['prefix'=>'/imagesproduct'], function () {
         Route::get('/product/{id}', [ImagesproductController::class, 'index']);
         Route::get('/{id}', [ImagesproductController::class, 'show']);
