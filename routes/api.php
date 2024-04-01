1c02daa90e26794fdde355e19c022b7f5222c308
victor.gualdron.r@gmail.com <vgualdron@estratec.local>
2024-04-01 15:00:33 -0500

    add endpoint search products 3

--- routes/api.php
+++ routes/api.php
@@ -75,7 +75,7 @@ Route::group(["prefix" => "/v1", "middleware" => ["auth:sanctum"]], function () 
 
 // endpoints tienda virtual
 Route::group(["prefix" => "/v1"], function () {
-    Route::group(['prefix'=>'/product'], function () {
+    Route::group(['prefix'=>'/home'], function () {
         Route::get('/search/params', [ProductController::class, 'search']);
     });
 });
